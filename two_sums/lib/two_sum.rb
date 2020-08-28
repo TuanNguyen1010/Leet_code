0c5e22acda2c2338598c27b6c21dd17adbc9e4d2
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-28 21:15:20 +0100

    Test: all test past

--- two_sums/lib/two_sum.rb
+++ two_sums/lib/two_sum.rb
@@ -1,13 +1,9 @@
 def two_sum(nums, target)
-  dic = {}
-  nums.each_with_index {|number, index|
-      dic[number] = index
-  }
-  
-  dic.each{| (k, v)|
-      result = target - nums[v]
-      if dic.has_key?(result) && k != result
-          return [v, dic[result]]
-      end 
-  }
-end
+    dict = {}
+    nums.each_with_index { |n, i|
+      if dict[target - n]
+        return dict[target - n], i
+      end
+      dict[n] = i
+    }
+  end
--- two_sums/spec/two_sum_spec.rb
+++ two_sums/spec/two_sum_spec.rb
@@ -7,6 +7,14 @@ describe '#two_sum' do
   end 
 
   it 'does not return the same element twice' do 
-    expect(two_sum([2, 2, 1, 3], 4)).to eq([2,3])
+    expect(two_sum([2, 2, 1, 3], 4)).to eq([0,1])
+  end 
+
+  it 'does not return the same index' do 
+    expect(two_sum([3,3], 6)).to eq([0,1])
+  end 
+  
+  it 'returns a [1,2] from input [3,2,4]' do 
+    expect(two_sum([3,2,4],6)).to eq([1,2])
   end 
 end
