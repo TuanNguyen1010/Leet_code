781e0272661f5e437afba3bd65468a0a7edf69dc
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-28 11:00:11 +0100

    Test: edge cases

--- two_sums/spec/two_sum_spec.rb
+++ two_sums/spec/two_sum_spec.rb
@@ -5,4 +5,8 @@ describe '#two_sum' do
   it 'takes an array with 2 numbers and return index' do 
     expect(two_sum([2,3], 5)).to eq([0,1])
   end 
+
+  it 'does not return the same element twice' do 
+    expect(two_sum([2, 2, 1, 3], 4)).to eq([2,3])
+  end 
 end
