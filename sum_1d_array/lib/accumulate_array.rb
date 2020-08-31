1b3c6d3ab6468f4737e50c9b7c10130a1ba57870
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-31 13:41:28 +0100

    Refactor: map array

--- sum_1d_array/lib/accumulate_array.rb
+++ sum_1d_array/lib/accumulate_array.rb
@@ -1,11 +1,9 @@
 
 def accumulate_array(array) 
   
-  if(array[3])
-    return [array[0], array[0] + array[1], array[0] + array[1] + array[2], array[0] + array[1] + array[2] + array[3]]
-  elsif(array[1]) 
-    return [array[0],array[0] + array[1]]
-  else
-    return array
-  end 
+  cummulative = 0
+  array.map{|number|
+    cummulative += number
+  }
+  
 end 
