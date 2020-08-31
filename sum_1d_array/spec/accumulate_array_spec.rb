3573ea030a3d68c95e1dea535ed5a20c81da146d
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-31 13:43:30 +0100

    Test: extra test cases

--- sum_1d_array/spec/accumulate_array_spec.rb
+++ sum_1d_array/spec/accumulate_array_spec.rb
@@ -12,4 +12,13 @@ describe '#accumulate_array' do
   it 'returns an accumulating array with input of 4 element array' do
     expect(accumulate_array([1,2,3,4])).to eq([1,3,6,10])
   end 
+
+  it 'returns an accumulating array with input of 5 element array' do
+    expect(accumulate_array([1,1,1,1,1])).to eq([1,2,3,4,5])
+  end 
+
+  it 'returns an accumulating array with input of 6 element array' do
+    expect(accumulate_array([3,1,2,10,1,1])).to eq([3,4,6,16,17,18])
+  end 
+  
 end 
